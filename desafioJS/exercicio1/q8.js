7e2c852bbe854ebbcd395288bb6d88c96760251d
Ricardo Espindola <86207903+RicardoEspindol@users.noreply.github.com>
2023-02-08 08:57:15 -0300

    concert

--- desafioJS/exercicio1/q8.js
+++ desafioJS/exercicio1/q8.js
@@ -1,41 +1,22 @@
 function preco_final(preco, opc){
     let text = '';
     if(opc === 1){
-        let result = (preco - (preco * 0.15)).toString().replace('.', ',');
-        if(result % 2 === 0 || result % 2 === 1){
-            text = 'R$ '+ result + ',00';
-            return text;
-        }else{
-            text = 'R$ '+ result;
-            return text;
-        }
+        let result = (preco - (preco * 0.15));
+        return "R$ " + result.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+       
     }else if(opc === 2){
-        let result = (preco - (preco * 0.10)).toString().replace('.', ',');
-        if(result % 2 === 0 || result % 2 === 1){
-            text = 'R$ '+ result + ',00';
-            return text;
-        }else{
-            text = 'R$ '+ result;
-            return text;
-        }
+        let result = (preco - (preco * 0.10));
+        return "R$ " + result.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+       
     }else if(opc === 3){
-        let result = (preco - (preco * 0.05)).toString().replace('.', ',');
-        if(result % 2 === 0 || result % 2 === 1){
-            text = 'R$ '+ result + ',00';
-            return text;
-        }else{
-            text = 'R$ '+ result;
-            return text;
-        }
+        let result = (preco - (preco * 0.05));
+        return "R$ " + result.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+       
     }else if(opc === 4){
-        let result = (preco - (preco * 0)).toString().replace('.', ',');
-        if(result % 2 === 0 || result % 2 === 1){
-            text = 'R$ '+ result + ',00';
-            return text;
-        }else{
-            text = 'R$ '+ result;
-            return text;
-        }
+        let result = (preco - (preco * 0));
+        return "R$ " + result.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }else{
+        return 'Opção de Desconto Inválida';
     }
 
 }
@@ -43,4 +24,3 @@ console.log(preco_final(100,1));
 console.log(preco_final(100,2));
 console.log(preco_final(100,3));
 console.log(preco_final(100,4));
-//console.log(preco_final(93.50,3));
--- desafioJS/exercicio2/q10.js
+++ desafioJS/exercicio2/q10.js
@@ -1,28 +1,28 @@
-function febre(array){
-    let saida = '[';
-    //let graus = ['"Normal"','"Hipotermia"','"Hipertermia"','"Febre"','"Febre Alta"'];
-    for(let i = 0; i < array.length; i++){
-        if((array[i]) <= 35){
-            saida += '"Hipotermia", ';
-        }else if((array[i]) > 35 && array[i] <= 37.7){
-            saida += '"Normal", ';
-        }else if((array[i]) > 37.7 && array[i] <= 39.5){
-            saida += '"Febre", ';
-        }else if((array[i]) > 39.5 && array[i] < 41){
-            saida += 'Febre Alta';
-        }else{
-            saida += '"Hipertemia", ';
+function febre(array)
+{
+    let dados=[];
+    for(let i=0;i<array.length;i++)
+    {
+        if(array[i]<35)
+        {
+            dados.push("Hipotermia");
         }
-        if(array[array.length-1]){
-            saida[saida.length] = '';
-            saida[saida.length-1] = '';
-
+        else if(array[i]<=37.7)
+        {
+            dados.push("Normal");
         }
-
+        else if(array[i]<=39.6)
+        {
+            dados.push("Febre");
+        }
+        else if(array[i]<=41)
+        {
+            dados.push("Febre Alta");
+        }
+        else
+        {
+            dados.push("Hipertermia");
+        }
     }
-    return saida + ']';
+    return dados;
 }
-let array = [35.41,33.12,41.06,38.9,40.22];
-console.log(febre(array))
-// let graus = ["Normal","Hipotermia","Hipertermia","Febre","Febre Alta"];
-// console.log(graus);
